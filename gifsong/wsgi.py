f3bdd1600fa16e9fbb33425128356014c7d5641f
Derek M. Anderson <toxigenicpoem@gmail.com>
2014-01-23 22:09:02 -0600

    updated wsgi

--- gifsong/wsgi.py
+++ gifsong/wsgi.py
@@ -1,14 +1,7 @@
-"""
-WSGI config for gifsong project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gifsong.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gifsong.settings")
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
