f4719c2607f0537900d04a655c9248e3e6db2b50
Derek M. Anderson <toxigenicpoem@gmail.com>
2014-01-28 12:41:57 -0600

    added sfw or unknown to random plays

--- gifsong/views.py
+++ gifsong/views.py
@@ -2,7 +2,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from django.forms import ModelForm
-
+from django.db.models import Q
 
 from gifsong.models import gifsong
 
@@ -10,38 +10,39 @@ class GifSongForm(ModelForm):
     class Meta:
         model = gifsong
         fields = ['image_url', 'audio_url']
-    
+
 class showgifsong(TemplateView):
     template_name = 'showsong.html'
 
     def get(self, request, *args, **kwargs):
         gvidid = request.GET.get('gvid')
         agifsong = None
-        
+
         if(gvidid):
             agifsong = gifsong.objects.get(id=gvidid)
-        
+
         if (agifsong == None):
-            if(gifsong.objects.order_by('?')):
-                agifsong = gifsong.objects.order_by('?')[0]
-        
+            sfw = gifsong.objects.all().filter(Q(sfwness=1) | Q(sfwness=3)).order_by('?')
+            if(sfw):
+                agifsong = sfw[0]
+
         context = {
             'song' : agifsong,
         }
-        
+
         return self.render_to_response(context)
 
 class addgifsong(TemplateView):
     template_name = 'createsong.html'
 
     def get(self, request, *args, **kwargs):
-        
-        form = GifSongForm() 
-        
+
+        form = GifSongForm()
+
         context = {
             'form' : form,
         }
-        
+
         return self.render_to_response(context)
 
     def post(self, request, *args, **kwargs):
@@ -49,5 +50,5 @@ class addgifsong(TemplateView):
         if form.is_valid():
             song = form.save()
             return redirect('/show?gvid=' + str(song.id))
-        
+
         return self.render_to_response({'form': GifSongForm() })
