660e8ce268a6f6b614d1ce413f96bf241a11ac96
Derek M. Anderson <toxigenicpoem@gmail.com>
2014-01-28 00:41:58 -0600

    added gif to django admin list display

--- gifsong/admin.py
+++ gifsong/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from gifsong.models import gifsong
 
 class gifsongAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('gif',)
+
+    def gif(self, obj):
+        return '<img style="width:100px;height:75px;" src="http://%s"/>' % (obj.image_url)
+    gif.allow_tags = True
+    gif.short_description = 'Gif Used'
+
 admin.site.register(gifsong, gifsongAdmin)
